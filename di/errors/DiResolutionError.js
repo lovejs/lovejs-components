42872e59b386d440c28e86566b83e85ba1561151
Vincent <vincent@fullstack.pro>
2018-07-09 16:45:21 +0200

    Fix error string for Di Resolution error

--- di/errors/DiResolutionError.js
+++ di/errors/DiResolutionError.js
@@ -2,7 +2,8 @@ const ExtendableError = require("es6-error");
 
 module.exports = class DiResolutionError extends ExtendableError {
     constructor(resolution, message, error) {
-        super(`Error resolving service "${resolution.getId()}" : ${message} ${error ? error.message : ''}\nDebug Stack: ${resolution.debugStack()}`);
+        const stack = resolution.isRoot ? "" : `\nDebug Stack: ${resolution.debugStack()}`;
+        super(`Error resolving service "${resolution.getId()}" : ${message} ${error ? error.message : ""} ${stack}`);
         this.wrappedError = error;
     }
 };
